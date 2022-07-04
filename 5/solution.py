f6d977e6c313e3e5cb0c46bcd9c2a13bfdcd2275
Elliot0122 <107611902+Elliot0122@users.noreply.github.com>
2022-07-04 17:02:53 +0800

    Update solution.py

--- 5/solution.py
+++ 5/solution.py
@@ -7,6 +7,9 @@ class Solution(object):
         ttl_length = len(s)
         ans_length = 1
         ans = s[0]
+        if ttl_length == 1:
+            return s
+        
         for index in range(ttl_length):
             length = 0
             while index - length >=0 and index + length < ttl_length:
@@ -18,11 +21,10 @@ class Solution(object):
                 ans = s[index-length:index+length+1]
                 
             length = 0
-            if ttl_length > 1:
-                while index - length >= 0 and index + length + 1 < ttl_length:
-                    if s[index-length] != s[index+length+1]: break
-                    length+=1
-                if ans_length < length*2:
-                    ans_length = length*2
-                    ans = s[index-length+1:index+length+1]
+            while index - length >= 0 and index + length + 1 < ttl_length:
+                if s[index-length] != s[index+length+1]: break
+                length+=1
+            if ans_length < length*2:
+                ans_length = length*2
+                ans = s[index-length+1:index+length+1]
         return ans
