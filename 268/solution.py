2d872efa57fca7a71aa7f0f152fd186d11b4e562
Elliot0122 <107611902+Elliot0122@users.noreply.github.com>
2022-07-24 15:21:59 +0800

    Create solution.py

---
+++ 268/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        length = len(nums)
+        return length*(length+1)/2 - sum(nums)
+        
