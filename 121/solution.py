156b539e198227ef96d60538adb4c497448a820d
Yi-Qing Lin <etlin@ucdavis.edu>
2025-02-13 14:14:31 -0800

    Update solution.py

--- 121/solution.py
+++ 121/solution.py
@@ -1,19 +1,12 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        
-        if len(prices) == 1:
-            return 0
-        minPrice = prices[0]
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minimum = prices[0]
         profit = 0
-        
+
         for i in prices:
-            minPrice = min(minPrice, i)
-            profit = max(profit, i-minPrice)
-        return profit
+            if i < minimum:
+                minimum = i
+            else:
+                profit = max(profit, i-minimum)
         
-            
-            
+        return profit
