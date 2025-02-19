4d7bb005858fc1adb9b0507a3551f1c93186a9df
Yi-Qing Lin <etlin@ucdavis.edu>
2025-02-18 16:15:09 -0800

    Create solution.py

---
+++ 274/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations = sorted(citations, reverse=True)
+        for i in range(len(citations)):
+            if i+1 > citations[i]:
+                return i
+        else:
+            return len(citations)
