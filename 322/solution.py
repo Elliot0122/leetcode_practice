b30ec4eb5b4cdeb80c8298b871990920b3b9e7b3
Yi-Qing Lin <etlin@ucdavis.edu>
2025-02-24 21:40:29 -0800

    Update solution.py

--- 322/solution.py
+++ 322/solution.py
@@ -1,16 +1,13 @@
-class Solution(object):
-    def coinChange(self, coins, amount):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
-        dp = [amount+1 for i in range(amount+1)]
-        dp[0] = 0
-        for i in range(amount+1):
-            for j in coins:
-                if i-j >=0:
-                    dp[i] = min(dp[i], dp[i-j]+1)
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        min_amount = [0]+[amount+1]*amount
 
-        return -1 if dp[amount] == amount+1 else dp[amount]
-                        
+        for i in range(1, amount+1):
+            for j in coins:
+                if i-j >= 0:
+                    min_amount[i] = min(min_amount[i], min_amount[i-j]+1)
+
+        if min_amount[-1] == amount+1:
+            return -1
+        else:
+            return min_amount[-1]
